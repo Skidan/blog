e8a70aa67c1240d44d7eff6d5d8c55af3d049b09
Dmytro <dmytro.skydanenko@gmail.com>
2018-05-10 20:35:09 +0300

    Delete main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-var domMenuBtn = document.getElementById("menu_icon");
-var domMainMenu = document.querySelector(".menu_list");
-
-function toggleMenu() {
-  domMenuBtn.classList.toggle("menuexpanded");
-  domMainMenu.classList.toggle("showmenu");
-}
-domMenuBtn.addEventListener("click", toggleMenu);
